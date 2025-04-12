d8eaa5efb543a39bdddefd6eac9b7b98ba73c0c1
root <root@DESKTOP-HsnMkt>
2025-04-13 04:12:41 +0700

    benekne cuaca

--- pages/api/cuaca.ts
+++ pages/api/cuaca.ts
@@ -1,14 +1,18 @@
 // pages/api/cuaca.ts
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { getWeather } from '../../lib/weatherAPI';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
-    const response = await fetch("https://api.open-meteo.com/v1/forecast?latitude=-7.4706&longitude=110.2178&daily=temperature_2m_max,temperature_2m_min,uv_index_max,precipitation_sum,precipitation_hours,uv_index_clear_sky_max&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m,wind_direction_10m&timezone=Asia%2FBangkok");
-    const data = await response.json();
-
-    return res.status(200).json({ cuaca: data });
+    const data = await getWeather(); // default Jogja
+    res.status(200).json({ 
+      success: true,
+      location: "Yogyakarta",
+      temperature: data.current_weather.temperature,
+      windspeed: data.current_weather.windspeed,
+      unit: "°C"
+    });
   } catch (error: any) {
-    console.error("Cuaca Error:", error.message);
-    return res.status(500).json({ error: "Gagal mengambil data cuaca" });
+    res.status(500).json({ success: false, message: error.message });
   }
 }
