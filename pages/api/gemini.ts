08f2839d795cb79e3d8c8b984c242df0b011f469
root <root@DESKTOP-HsnMkt>
2025-05-30 20:13:04 +0700

    fetch tunggal

--- pages/api/gemini.ts
+++ pages/api/gemini.ts
@@ -6,8 +6,7 @@ import path from "path";
 
 /* ── Konstanta CORS ── */
 const ORIGIN = [
-  "https://portofoliomukti.framer.website",
-  "https://portofolioku2-astro-theme.vercel.app/ShazQIAI",
+  "https://portofolioku2-astro-theme.vercel.app",
 ];
 const METHODS = "POST, OPTIONS";
 const HEADERS = "Content-Type";
