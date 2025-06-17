aad349c63f4f7e59e8ea20dd29248bc6b147e9e4
root <root@DESKTOP-HsnMkt.localdomain>
2025-06-18 03:14:05 +0700

    all

--- utils/cors.ts
+++ utils/cors.ts
@@ -7,7 +7,7 @@ const cors = Cors({
   methods: ["POST", "OPTIONS"],
   origin: ["https://portofoliomukti.framer.website",
             "https://portofolioku2-astro-theme.vercel.app",
-            "https://portofolioku-v2.vercel.app/"] // Ganti dengan domain Framer kamu kalau mau lebih aman
+            "https://portofolioku-v2.vercel.app"] // Ganti dengan domain Framer kamu kalau mau lebih aman
 });
 
 function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: Function) {
